7cde1f60b7b69f0cbc3f999bd6193463a7421cc8
gvamsikrishna71817@gmail.com <gvamsikrishna71817@gmail.com>
2024-12-27 13:07:41 +0530

    updatedBilling

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -15,12 +15,12 @@ const Billing = () => {
             <h2 className = {styles.heading2}>
                 Easily control your <br className = "sm:block hidden"/> billing & invoicing.
             </h2>
-            <p className = {`${styles.paragraph} max-2-[470px] mt-5`}>
+            <p className = {`${styles.paragraph} max-w-[470px] mt-5`}>
                 Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aeneanneque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
             </p>
 
             <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-                <img src={apple} alt="apple_store" className = "w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
+                <img src={apple} alt="apple_store" className = "w-[128px] h-[42px] object-contain mr-5 first:mb-5 sm:first:mb-0 cursor-pointer"/>
                 <img src={google} alt="google_play" className = "w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
             </div>
         </div>
